66c8b0fb24386b7ab83dcb9f96e0065c7211450d
Sudarshan Iyer <145035065+sudu-sr7@users.noreply.github.com>
2026-06-04 19:17:57 +0530

    Single render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,22 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-
-import connectDB
-from "./config/db.js";
+import connectDB from "./config/db.js";
 
 connectDB();
 
-app.listen(
+const PORT =
+  process.env.PORT || 5000;
 
-  process.env.PORT,
-
-  () => {
-
-    console.log(
-      `Server Running On Port ${process.env.PORT}`
-    );
-
-  }
-
-);
+app.listen(PORT, () => {
+  console.log(
+    `Server Running On Port ${PORT}`
+  );
+});
